0daf5ce3f5da810aa8b132b61307ff9ae6acbc6f
Nolan Greig <ngreig85@comcast.net>
2016-01-31 10:01:16 -0500

    Began NetworkTables implementation

--- Arnold-5/src/org/usfirst/frc/team4546/robot/Robot.java
+++ Arnold-5/src/org/usfirst/frc/team4546/robot/Robot.java
@@ -6,6 +6,8 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
 //import org.usfirst.frc.team4546.robot.commands.ExampleCommand;
 import org.usfirst.frc.team4546.robot.subsystems.Drivetrain;
 import org.usfirst.frc.team4546.robot.subsystems.TestStuff;
@@ -29,6 +31,7 @@ public class Robot extends IterativeRobot {
 	public static OI oi;
 	
 	CameraServer camera;
+	NetworkTable table;
 
     Command autonomousCommand;
     SendableChooser chooser;
@@ -53,6 +56,8 @@ public class Robot extends IterativeRobot {
 //        chooser.addObject("My Auto", new MyAutoCommand());
         
         SmartDashboard.putData("Auto mode", chooser);
+        table = NetworkTable.getTable("Camera");
+        
     }
 	
 	/**
@@ -69,33 +74,11 @@ public class Robot extends IterativeRobot {
 		Scheduler.getInstance().run();
 	}
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
-	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
-	 * Dashboard, remove all of the chooser code and uncomment the getString code to get the auto name from the text box
-	 * below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional commands to the chooser code above (like the commented example)
-	 * or additional comparisons to the switch structure below with additional strings & commands.
-	 */
 	
     public void autonomousInit() {
         autonomousCommand = (Command) chooser.getSelected();
-        
-		/* String autoSelected = SmartDashboard.getString("Auto Selector", "Default");
-		switch(autoSelected) {
-		case "My Auto":
-			autonomousCommand = new MyAutoCommand();
-			break;
-		case "Default Auto":
-		default:
-			autonomousCommand = new ExampleCommand();
-			break;
-		} */
-    	
-    	// schedule the autonomous command (example)
-        
         if (autonomousCommand != null) autonomousCommand.start();
+        
     }
 
     /**
@@ -120,11 +103,19 @@ public class Robot extends IterativeRobot {
     
     public void teleopPeriodic() {
         Scheduler.getInstance().run();
-        SmartDashboard.putNumber("US Sensor Voltage", testStuff.getVoltage());
-        SmartDashboard.putNumber("US Sensor Value",  testStuff.getValue());
+        
         SmartDashboard.putNumber("Value Distance", testStuff.getValueDistance());
         SmartDashboard.putNumber("Voltage Distance", testStuff.getVoltageDistance());
         SmartDashboard.putNumber("Drivetrain AHRS Angle", drivetrain.getAngle());
+        SmartDashboard.putNumber("distance", 0);
+        SmartDashboard.putNumber("NORTHEAST_X", 0);
+        SmartDashboard.putNumber("NORTHEAST_Y", 0);
+        SmartDashboard.putNumber("NORTHWEST_X", 0);
+        SmartDashboard.putNumber("NORTHWEST_Y", 0);
+        SmartDashboard.putNumber("SOUTHEAST_X", 0);
+        SmartDashboard.putNumber("SOUTHEAST_Y", 0);
+        SmartDashboard.putNumber("SOUTHWEST_X", 0);
+        SmartDashboard.putNumber("SOUTHWEST_Y", 0);
     }
     
     /**

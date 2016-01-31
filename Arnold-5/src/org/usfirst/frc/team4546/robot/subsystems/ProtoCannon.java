ec81665c7bb352b1e43142f16c6831482727a900
Nolan Greig <ngreig85@comcast.net>
2016-01-31 10:01:45 -0500

    Began cannon

---
+++ Arnold-5/src/org/usfirst/frc/team4546/robot/subsystems/ProtoCannon.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team4546.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class ProtoCannon extends Subsystem {
+
+	protected void initDefaultCommand() {
+		
+	}
+
+}

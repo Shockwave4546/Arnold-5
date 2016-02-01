b84856b59860e4619e03313f1d142a75e72a1e21
Skatastica <skatastica@yahoo.com>
2016-02-01 14:57:41 -0500

    cool pid class

---
+++ Arnold-5/src/org/usfirst/frc/team4546/robot/commands/PID.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team4546.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class PID extends Command {
+
+	protected void initialize() {
+
+	}
+
+	protected void execute() {
+
+	}
+
+	protected boolean isFinished() {
+		return false;
+	}
+
+	protected void end() {
+
+	}
+
+	protected void interrupted() {
+
+	}
+
+}

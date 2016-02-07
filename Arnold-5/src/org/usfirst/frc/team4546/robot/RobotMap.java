b29adfe05b8f43eb0eb94daa9f26fb1342ae68cd
ShockwaveComputer <dovershockwave@gmail.com>
2016-02-07 15:40:09 -0500

    Finished getDistance() Method to AimToFire

--- Arnold-5/src/org/usfirst/frc/team4546/robot/RobotMap.java
+++ Arnold-5/src/org/usfirst/frc/team4546/robot/RobotMap.java
@@ -5,10 +5,12 @@ import edu.wpi.first.wpilibj.Servo;
 
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.interfaces.Potentiometer;
 
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
@@ -42,7 +44,7 @@ public class RobotMap {
 	public static AnalogInput testStuffUSSensor;
 	public static Servo protoCannonFeedBall;
 	public static DigitalInput protoCannonLimitSwitch;
-	//public static DigitalInput protoCannonAimButton;
+	public static Potentiometer protoCannonPotentiometer;
 	
 	public static void init()	{
 		
@@ -71,7 +73,7 @@ public class RobotMap {
         protoCannonFireRight = new Talon(7);
         protoCannonFeedBall = new Servo(8);
         protoCannonLimitSwitch = new DigitalInput(9);
-        //protoCannonAimButton = new DigitalInput(10);
+        protoCannonPotentiometer = new AnalogPotentiometer(10, 360, 30); //http://wpilib.screenstepslive.com/s/4485/m/13809/l/241877-potentiometers-measuring-joint-angle-or-linear-motion
         
         drivetrainAHRS = new AHRS(SPI.Port.kMXP);
                 
--- Arnold-5/src/org/usfirst/frc/team4546/robot/commands/AimToFire.java
+++ Arnold-5/src/org/usfirst/frc/team4546/robot/commands/AimToFire.java
@@ -1,9 +1,44 @@
 package org.usfirst.frc.team4546.robot.commands;
 
+import org.usfirst.frc.team4546.robot.RobotMap;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 public class AimToFire extends Command {
 
+
+	
+	double theta = RobotMap.protoCannonPotentiometer.get();
+	double sinTheta = Math.sin(theta);
+	double theta2 = 90 - theta;
+	
+	double cannonHeight;	//Physically Measured
+	double PMHeight;		//Physically Measured
+	double dEOCToCamera;	//Physically Measured
+	
+	public double getHPMToEOC() {
+		return Math.sin(theta2) * cannonHeight;
+	}
+	public double getHCameraToEOC() {
+		return dEOCToCamera/Math.sin(theta);
+	}
+	
+	double hPMToEOC = getHPMToEOC();
+	double hCameraToEOC = getHCameraToEOC();
+
+
+	public double getCameraHeight() {
+		return hPMToEOC + PMHeight + hPMToEOC;
+	}
+	
+	double cameraHeight = getCameraHeight();
+	
+	public double getDistance() {
+		return (91 - cameraHeight)/sinTheta;
+	}
+	
+	double distance = getDistance();
+	
 	public AimToFire() {
 		
 	}
